98dea8f0ee5fe8e6943722616173bb9221ca6bdb
Mikhail <novoselovzab@gmail.com>
2024-01-04 04:00:54 +0900

    Expanded pwm signal width

--- src/lib/ServoOutput/devServoOutput.cpp
+++ src/lib/ServoOutput/devServoOutput.cpp
@@ -74,6 +74,7 @@ static void servoWrite(uint8_t ch, uint16_t us)
         }
         else
         {
+            if (ch != 2) us = fmap(us, 988, 2012, 700, 2300);
             PWM.setMicroseconds(pwmChannels[ch], us / (chConfig->val.narrow + 1));
         }
     }
